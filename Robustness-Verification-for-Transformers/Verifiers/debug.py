3f56cb277d9bdcf20627163325f4a511f0575515
pauljiang03 <112199352+pauljiang03@users.noreply.github.com>
2025-04-03 22:09:00 -0400

    Delete Robustness-Verification-for-Transformers/Verifiers/debug.py

--- Robustness-Verification-for-Transformers/Verifiers/debug.py
+++
@@ -1,7 +0,0 @@
-def print_tensors_info(variables):
-    for name, var in variables.items():
-        try:
-            dev = var.device
-            print("%s - device '%s'" % (name, dev))
-        except AttributeError:
-            pass
