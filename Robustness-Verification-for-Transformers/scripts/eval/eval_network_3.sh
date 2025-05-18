bb9084c394efb743ca13b4a73bed728667d9b7ca
pauljiang03 <112199352+pauljiang03@users.noreply.github.com>
2025-05-18 03:42:56 -0400

    Delete Robustness-Verification-for-Transformers/scripts/eval directory

--- Robustness-Verification-for-Transformers/scripts/eval/eval_network_3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd ../..
-
-nlayers=3
-echo "Verifying network with $nlayers layers - Zonotope slow"
-
-python3 main.py \
-      --data sst \
-      --dir sst_bert_small_$nlayers --num_layers $nlayers
